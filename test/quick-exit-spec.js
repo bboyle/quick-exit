edccf39080cafe11b3fb5eeaaa81fea2d01fedc1
Ben Boyle <benjamins.boyle@gmail.com>
2016-01-11 09:41:46 +1000

    add tests for unload event
    test that unload event does not break quick exit behaviour

--- test/quick-exit-spec.js
+++ test/quick-exit-spec.js
@@ -1,12 +1,13 @@
 /*global casper */
-var url = 'http://localhost:9999/test/test.html';
+const TEST_URL = 'http://localhost:9999/test/test.html';
+const TEST_URL_UNLOAD = 'http://localhost:9999/test/unload.html';
 
 
 casper.test.begin( 'quick exit (click)', 4, function suite( test ) {
 
 	casper.start()
 
-	.thenOpen( url )
+	.thenOpen( TEST_URL )
 	.then(function() {
 		test.assertExists( 'a#quick-exit', 'Quick exit link is present' );
 
@@ -33,7 +34,7 @@ casper.test.begin( 'quick exit (keyboard)', 5, function suite( test ) {
 
 	casper.start()
 
-	.thenOpen( url )
+	.thenOpen( TEST_URL )
 	.then(function() {
 		test.assertExists( 'a#quick-exit', 'Quick exit link is present' );
 		test.assertExists( 'a#quick-exit[accesskey=q]', 'accesskey is Q' );
@@ -61,7 +62,7 @@ casper.test.begin( 'quick exit (keyboard - forms integration)', 8, function suit
 
 	casper.start()
 
-	.thenOpen( url )
+	.thenOpen( TEST_URL )
 	.then(function() {
 		test.assertExists( 'a#quick-exit[accesskey=q]', 'accesskey is Q' );
 
@@ -78,6 +79,33 @@ casper.test.begin( 'quick exit (keyboard - forms integration)', 8, function suit
 		this.click( 'select[name=select]' ); // focus for sendKeys
 		this.sendKeys( 'select[name=select]', 'q' );
 		test.assertField( 'select', 'Queensland', 'accesskey ignored for select' );
+	})
+
+	.run(function() {
+		test.done();
+	});
+});
+
+
+casper.test.begin( 'quick exit (unload)', 4, function suite( test ) {
+
+	casper.start()
+
+	.thenOpen( TEST_URL_UNLOAD )
+	.then(function() {
+		test.assertExists( 'a#quick-exit', 'Quick exit link is present' );
+
+		this.click( 'a#quick-exit' );
+		test.assertElementCount( '*', 0, 'Page is empty after quick exit click' );
+	})
+
+	.then(function() {
+		test.assertUrlMatch( /^http:\/\/www\.google\.com/, 'Landed on google.com after clicking quick exit' );
+	})
+
+	.back()
+	.then(function() {
+		test.assertUrlMatch( /^http:\/\/localhost:9999\/$/, 'Landed on localhost:9999/ after back()' );
 	})
 
 	.run(function() {
